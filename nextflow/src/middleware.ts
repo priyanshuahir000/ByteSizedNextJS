f9df634e6553ee3e9d45ca66a0a05e8bb7ed7b9c
priyanshuahir000 <priyanshuahir01@gmail.com>
2025-03-25 17:32:43 +0530

    Add immer and zustand dependencies, and implement middleware for database and storage initialization

---
+++ nextflow/src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+import getOrCreateDB from "./models/server/dbSetup";
+import getOrCreateStorage from "./models/server/storageSetup";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  await Promise.all([getOrCreateDB(), getOrCreateStorage()]);
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  /*
+   * Match all request paths except for:
+   * - api (API routes)
+   * - _next/static (static files)
+   * - _next/image (image optimization files)
+   * - favicon.ico (favicon file)
+   */
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
