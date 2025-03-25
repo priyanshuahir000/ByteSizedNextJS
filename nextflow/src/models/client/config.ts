639a253a7babd18a269df85a4e2da555fa9f80e9
priyanshuahir000 <priyanshuahir01@gmail.com>
2025-03-25 15:26:46 +0530

    Add initial project setup with configuration files, environment variables, and SVG assets

---
+++ nextflow/src/models/client/config.ts
@@ -0,0 +1,21 @@
+import env from "@/app/env";
+import { Client, Account, Avatars, Databases, Storage } from "appwrite";
+
+const client = new Client()
+  .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+  .setProject(env.appwrite.projectId); // Your project ID
+
+const databases = new Databases(client);
+const account = new Account(client);
+const avatars = new Avatars(client);
+const storage = new Storage(client);
+
+export {
+    client,
+    account,
+    avatars,
+    databases,
+    storage,
+};
+
+
---
+++ nextflow/src/models/name.ts
@@ -0,0 +1,6 @@
+export const db = "main-nextflow";
+export const questionCollection = "question";
+export const answerCollection = "answer";
+export const commentsCollection = "comments";
+export const votesCollection = "votes";
+export const questionAttachmentBucket = "question-attachment";
